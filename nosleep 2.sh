718f9ac6945f05e19afcd6efdc799494e71a34a7
Thomas Simmons <g.thomas.simmons@gmail.com>
2021-07-23 11:43:03 -0700

    removed redundant files

--- nosleep 2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-cd /Users/thomas_simmons/Documents/Python/automate/
-source /Users/thomas_simmons/.poetry/virtualenvs/automate-RISY0B_m-py3.9/bin/activate
-python nosleep.py
--- share 2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-cd $HOME/Desktop/shared
-ifconfig | grep 192.168
-python -m http.server
