1f55c2285e16aaff125cb8df081aaefc47a9aea7
Thomas Simmons <g.thomas.simmons@gmail.com>
2021-12-23 11:23:00 -0800

    Updated wayfinding UI

--- kmix.py
+++ kmix.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import subprocess
-
+import platform
 
 def main():
     """ User inputs the brand name and keywords, and the script will create a new
     list of words containing the brand name and keywords formatted for google ads
     and copy it to the clipboard."""
+
     brandname = input("Enter comma seperated branded terms: ")
     keywords = input("Enter comma seperated product keywords: ")
     brnds = [brnd.strip() for brnd in brandname.split(',')]
@@ -23,9 +24,18 @@ def main():
 
     return new_list
 
+def copy_to_clipboard(sorted_keyword_list):
+    """ Copy the new list to the clipboard."""
+    text_to_clipboard = '\n'.join(sorted_keyword_list)
+    if platform.system() == 'Darwin':
+        subprocess.run(['pbcopy'], input='\n'.join(text_to_clipboard))
+    elif platform.system() == 'Windows':
+        subprocess.run(['clip'], input='\n'.join(text_to_clipboard))
+    else:
+        subprocess.run(['xsel', '-ib'], input='\n'.join(text_to_clipboard))
 
 if '__main__' == __name__:
     sorted_keyword_list = main()
-    text_to_clipboard = '\n'.join(sorted_keyword_list)
-    subprocess.run("pbcopy", universal_newlines=True, input=text_to_clipboard)
-    print('Copied new keywords to clipboard!')
+    copy_to_clipboard(sorted_keyword_list)
+    print('\n'.join(sorted_keyword_list))
+    print('\nCopied new keywords to clipboard. Paste into google ads with CMD+V')
